b10dc410cc3d9a3abefaa9c23de4a0508af60a73
Yan Asadchy <focusrs29@gmail.com>
2018-04-28 19:51:37 +0300

    Replaced fragments of project creation screens with activities

--- app/src/main/java/com/example/focus/e_democracy/ProjectBuilderPageTwo.java
+++ app/src/main/java/com/example/focus/e_democracy/ProjectBuilderPageTwo.java
@@ -1,22 +1,18 @@
 package com.example.focus.e_democracy;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ProjectBuilderActivity extends AppCompatActivity {
+public class ProjectBuilderPageTwo extends AppCompatActivity {
 
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_project_builder);
-        getSupportActionBar().setTitle("Create proposals");
+        setContentView(R.layout.fragment_createproject2);
+        getSupportActionBar().setTitle("Create proposal");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
 
     }
     @Override  //--Method to enable menu on top bar
@@ -25,7 +21,8 @@ public class ProjectBuilderActivity extends AppCompatActivity {
         getMenuInflater().inflate( R.menu.menu_tabbed, menu );
         return true;
     }
-    @Override   //--Method to control buttons on top bar
+
+    @Override  //--Method to control buttons on top bar
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             // Respond to the action bar's Up/Home button
@@ -33,8 +30,8 @@ public class ProjectBuilderActivity extends AppCompatActivity {
                 NavUtils.navigateUpFromSameTask(this);
                 return true;
             case R.id.next:
-                Intent intent = new Intent(getApplicationContext(), ProjectBuilderPageTwo.class);
-                startActivity(intent);
+                //Intent intent = new Intent(getApplicationContext(), ProjectBuilderPageTwo.class);
+                //startActivity(intent);
                 return true;
         }
         return super.onOptionsItemSelected(item);
