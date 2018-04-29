bf3c32ee0fe6669a5302c650de45a6db291fd3c7
Yan Asadchy <focusrs29@gmail.com>
2018-04-29 19:44:18 +0300

    Array list, visual bugs

--- app/src/main/java/com/example/focus/e_democracy/ProjectBuilderPageTwo.java
+++ app/src/main/java/com/example/focus/e_democracy/ProjectBuilderPageTwo.java
@@ -8,7 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class ProjectBuilderPageTwo extends AppCompatActivity {
 
@@ -17,6 +19,17 @@ public class ProjectBuilderPageTwo extends AppCompatActivity {
         setContentView(R.layout.fragment_createproject2);
         getSupportActionBar().setTitle("Create proposal");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Array
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
 
         //add location button code
 
